74af573895a0eb76e3066397b0727c56359afb1a
gdgfgf123-creator <gdgfgf123@gmail.com>
2025-12-02 15:57:08 +0700

    Save

--- Assets/Code/Item.cs
+++ Assets/Code/Item.cs
@@ -4,6 +4,7 @@ public abstract class Item : MonoBehaviour
 {
     Player player;
     
-    public abstract void OnTriggerEnter2D(Collider2D collision);
+    public abstract void OnTriggerEnter2D(Player player);
+   
     
 }
--- Assets/Code/Seed Boost.cs
+++ Assets/Code/Seed Boost.cs
@@ -4,7 +4,7 @@ public class SeedBoost : Item
 {
     Player player;
     [field : SerializeField]public float newSpeed;
-    public override void OnTriggerEnter2D(Collider2D collision)
+    public override void OnTriggerEnter2D(Player player)
     {
         player.SetMoveSpeed(newSpeed);
     }
